6f64688aa9ff37c1cb41424cec6e7930273c8d1e
konstantinrts <k.developer@acceptance.ltd>
2024-10-24 15:18:14 +0300

    all files 04

---
+++ src/app/career/page.jsx
@@ -0,0 +1,14 @@
+import CareerApply from "./_components/CareerApply";
+import CareerHero from "./_components/CareerHero";
+import CareerWhy from "./_components/CareerWhy";
+
+
+export default function Career() {
+    return (
+        <>
+            <CareerHero />
+            <CareerWhy />
+            <CareerApply />
+        </>
+    );
+}
