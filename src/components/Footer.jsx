574bec287df603ab8f4e1b8dead6d4738270a2f4
SergWebAcceptance <serg.web@acceptance.ltd>
2024-11-18 14:22:52 +0200

    added footer address

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import "@/styles/footer.scss";
 import Link from "next/link";
 
@@ -9,38 +9,64 @@ const Footer = () => {
       <div className="footer__container _container">
         <div className="footer__body">
           <div className="footer__row-01">
-            <Link href="/" className="footer__logo"><img src="/logo-white.svg" alt="logo" /></Link>
+            <Link href="/" className="footer__logo">
+              <img src="/logo-white.svg" alt="logo" />
+            </Link>
             <div className="footer__wrapper">
               <div className="footer__col-01">
                 <h2 className="footer__title">Zenplex Digital Ltd</h2>
-                <div className="footer__text">Business and Marketing Consultancy Leading You to Success.</div>
+                <div className="footer__text">
+                  Business and Marketing Consultancy Leading You to Success.
+                </div>
               </div>
               <div className="footer__col-02">
                 <nav className="footer__nav">
                   <ul className="footer__menu-list">
                     <li className="footer__menu-item">
-                      <Link href="/business-consulting" className="footer__menu-link">Business Consulting</Link>
+                      <Link
+                        href="/business-consulting"
+                        className="footer__menu-link"
+                      >
+                        Business Consulting
+                      </Link>
                     </li>
                     <li className="footer__menu-item">
-                      <Link href="/log-in" className="footer__menu-link">Account</Link>
+                      <Link href="/log-in" className="footer__menu-link">
+                        Account
+                      </Link>
                     </li>
                     <li className="footer__menu-item">
-                      <Link href="/help" className="footer__menu-link">FAQ</Link>
+                      <Link href="/help" className="footer__menu-link">
+                        FAQ
+                      </Link>
                     </li>
                     <li className="footer__menu-item">
-                      <Link href="/our-approach" className="footer__menu-link">Our Approach</Link>
+                      <Link href="/our-approach" className="footer__menu-link">
+                        Our Approach
+                      </Link>
                     </li>
                     <li className="footer__menu-item">
-                      <Link href="/marketing-consulting" className="footer__menu-link">Marketing Consulting</Link>
+                      <Link
+                        href="/marketing-consulting"
+                        className="footer__menu-link"
+                      >
+                        Marketing Consulting
+                      </Link>
                     </li>
                     <li className="footer__menu-item">
-                      <Link href="/cart" className="footer__menu-link">Your Cart</Link>
+                      <Link href="/cart" className="footer__menu-link">
+                        Your Cart
+                      </Link>
                     </li>
                     <li className="footer__menu-item">
-                      <Link href="/help" className="footer__menu-link">Insights</Link>
+                      <Link href="/help" className="footer__menu-link">
+                        Insights
+                      </Link>
                     </li>
                     <li className="footer__menu-item">
-                      <Link href="/contact" className="footer__menu-link">Contact</Link>
+                      <Link href="/contact" className="footer__menu-link">
+                        Contact
+                      </Link>
                     </li>
                   </ul>
                 </nav>
@@ -49,15 +75,29 @@ const Footer = () => {
                 <ul className="footer__contacts">
                   <li className="footer__contact">
                     Email:{" "}
-                    <Link href="mailto:info@velloxia.com" className="footer__link">info@velloxia.com</Link>
+                    <Link
+                      href="mailto:info@velloxia.com"
+                      className="footer__link"
+                    >
+                      info@velloxia.com
+                    </Link>
                   </li>
                   <li className="footer__contact">
-                    Phone:{" "}
-                    <Link href="#" className="footer__link"></Link>
+                    Phone: <Link href="#" className="footer__link"></Link>
                   </li>
                   <li className="footer__contact">
-                    Address:{" "}
-                    <Link href="#" className="footer__link"></Link>
+                    Registered address:{" "}
+                    <Link href="#" className="footer__link">
+                      Nissi, 68 Agia Napa, <br />
+                      2044, Famagusta, Cyprus
+                    </Link>
+                  </li>
+                  <li className="footer__contact">
+                    Office address:{" "}
+                    <Link href="#" className="footer__link">
+                      Office 61, Floor 6, Roussos Centre <br />
+                      Gladstone Str 55, Limassol, 3040, Cyprus
+                    </Link>
                   </li>
                 </ul>
               </div>
@@ -67,29 +107,40 @@ const Footer = () => {
             <nav className="footer__policy">
               <ul className="footer__policy-list">
                 <li className="footer__policy-item">
-                  <Link href="/terms-and-conditions" className="footer__policy-link">Terms and Conditions</Link>
+                  <Link
+                    href="/terms-and-conditions"
+                    className="footer__policy-link"
+                  >
+                    Terms and Conditions
+                  </Link>
                 </li>
                 <li className="footer__policy-item">
-                  <Link href="/privacy-policy" className="footer__policy-link">Privacy Policy</Link>
+                  <Link href="/privacy-policy" className="footer__policy-link">
+                    Privacy Policy
+                  </Link>
                 </li>
                 <li className="footer__policy-item">
-                  <Link href="/cookie-policy" className="footer__policy-link">Cookie Policy</Link>
+                  <Link href="/cookie-policy" className="footer__policy-link">
+                    Cookie Policy
+                  </Link>
                 </li>
                 <li className="footer__policy-item">
-                  <Link href="/refund-policy" className="footer__policy-link">Refund Policy</Link>
+                  <Link href="/refund-policy" className="footer__policy-link">
+                    Refund Policy
+                  </Link>
                 </li>
               </ul>
             </nav>
           </div>
           <div className="footer__row-03">
             <div className="footer__copy">
-              © {currentYear} Zenplex Digital Ltd. All rights reserved.
+              © {currentYear} NOSCEMO LTD. All rights reserved.
             </div>
           </div>
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
